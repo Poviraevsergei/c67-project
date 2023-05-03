f5e2eb2e6b5eb8d4fded21b3c157888a32c418bb
Poviraevsergei <pacmanmozg1441>
2023-05-03 21:22:17 +0300

    My homework! From second branch

--- src/lesson_5/MyHomeWork.java
+++ src/lesson_5/MyHomeWork.java
@@ -3,5 +3,6 @@ package lesson_5;
 public class MyHomeWork {
     public static void main(String[] args) {
         System.out.println("My home 5");
+        System.out.println("My home 5 from new branch");
     }
 }
