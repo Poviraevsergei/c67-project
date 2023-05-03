246e8dd584dab0aaeacaa18deacd13a4e5e58a4e
Poviraevsergei <pacmanmozg1441>
2023-05-03 21:46:48 +0300

    My homework! From second branch 51

--- src/lesson_5/MyHomeWork.java
+++ src/lesson_5/MyHomeWork.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class MyHomeWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("My home 11 from new branch");
+
+        System.out.println("My sdfbla sdffrom new branch");
+        System.out.println("Hello tipa");
     }
 }
