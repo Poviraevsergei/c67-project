197cb6a781f2481818651411830ccf650df55806
Poviraevsergei <pacmanmozg1441>
2023-05-03 21:47:28 +0300

    My homework! From second branch 51

--- src/lesson_5/MyHomeWork.java
+++ src/lesson_5/MyHomeWork.java
@@ -6,5 +6,6 @@ public class MyHomeWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("My home 11 from new branch");
+        System.out.println("WORD");
     }
 }
